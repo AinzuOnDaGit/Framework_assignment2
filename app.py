8d30f161f85c11dc4df06281169c0631c1af6fc0
WillT04X <willjtanner04@gmail.com>
2026-05-05 15:49:58 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 app = Flask(
 __name__,
-template_folder="../client/templates", # define the route for pages
+template_folder="./templates", # define the route for pages
 )
 app.secret_key = "secretkeyforapplication"
