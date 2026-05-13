7295a9d4fc02783b4d6e8e08495f4bbf1441222c
Will Tanner <WT58@canterbury.ac.uk>
2026-05-13 19:08:32 +0100

    file uploads

--- Django/blog/forms.py
+++ Django/blog/forms.py
@@ -4,9 +4,8 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["title","description","image_filename"]
+        fields = ["title","description","image"]
         widgets = {
             "title": forms.TextInput(attrs={"class": "form-control", "required": True}),
             "description": forms.TextInput(attrs={"class": "form-control", "required": True}),
-            "image_filename": forms.TextInput(attrs={"class": "form-control"}),
         }
