9e5be268018527a7598e467564284403ff05bd41
Will Tanner <WT58@canterbury.ac.uk>
2026-05-13 19:28:51 +0100

    image uploading

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     description = models.TextField(max_length=100)
+    #images
     image = models.ImageField(upload_to="images/", null=True, blank=True)
 
 # Django recommends defining __str__, provides human-readable representation of the model instance.
