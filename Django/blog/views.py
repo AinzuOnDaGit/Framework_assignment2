1b8aa4137bd4ff9963e6a7c0950f1a8a7db4f1f0
Will Tanner <WT58@canterbury.ac.uk>
2026-05-13 16:45:08 +0100

    search functionality Django

--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
+from django.db.models import Q # allows or/and/not queries in Django
 from .forms import PostForm
 #import models inside the app
 from .models import Post
 
 # Create your views here.
 def index(request):
-    posts = Post.objects.all()
+    # retrieve the search item from the "Filter" parameter
+    query = request.GET.get("Filter")
+
+    if query:
+        # use Q to allow OR logic
+        # title or description contains query (passed searched item)
+        posts = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(description__icontains=query)
+        ).order_by('-id')
+    else:
+        posts = Post.objects.order_by('id')[:3]
     return render(request, "blog/index.html", {"posts": posts})
 
 def create_post(request):
