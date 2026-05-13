45658f668b57aacae2268b07f9147b660cdf288f
Will Tanner <WT58@canterbury.ac.uk>
2026-05-13 03:27:43 +0100

    bootstrap integration and Post creation

--- Django/blog/models.py
+++ Django/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField(max_length=100)
+    image_filename = models.CharField(max_length=100, null=True, blank=True)
+
+# Django recommends defining __str__, provides human-readable representation of the model instance.
+# improves debugging throughout Django admin interface.
+    def __str__(self):
+        return self.post.title
--- Django/blog/views.py
+++ Django/blog/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from .forms import PostForm
+#import models inside the app
+from .models import Post
 
 # Create your views here.
 def index(request):
     return render(request, "blog/index.html")
+
+def create_post(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        
+        if form.is_valid():
+            form.save
+            return redirect("index")
+    else:
+        form = PostForm()
+        
+    return render(request, "blog/create.html", {"form": form})
